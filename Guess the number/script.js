dcb6a0023d1ee11e50e223aa624428e960341b9d
hrithik021 <hrithikpvkr@gmail.com>
2021-09-02 14:48:32 +0530

    Guess My Number Game

---
+++ Guess the number/script.js
@@ -0,0 +1,47 @@
+'use strict';
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highscore = 20;
+
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+
+  if (!guess) {
+    displayMessage('🚫 No number');
+  } else if (guess === secretNumber) {
+    displayMessage('😍 Correct number');
+    document.querySelector('.number').textContent = secretNumber;
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      displayMessage(guess > secretNumber ? '😒 Too high!' : '😂 Too low');
+      score--;
+      document.querySelector('.score').textcontent = score;
+    } else {
+      displayMessage('👌 oops You lost the game');
+      document.querySelector('.score').textcontent = 0;
+    }
+  }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  displayMessage('Start guessing...');
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.guess').value = '';
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+});
